b36d56577e3b2c0f63077fa8785af104e5c1bd35
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-23 20:56:10 +0530

    Update leaderbinclude.php

--- leaderbinclude.php
+++ leaderbinclude.php
@@ -21,8 +21,23 @@ if($conn === false){
   echo "<br>";
 }
 
+function getRating(platform, user){
+	$url = 'https://competitive-coding-api.herokuapp.com/api/'.platform.'/'.user;
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HTTPGET, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$response_json = curl_exec($ch);
+	curl_close($ch);
+	$response=json_decode($response_json, true);
+	return $response['rating'];
+}
+
+
+
 $sql1 = "SELECT * FROM persons ORDER BY score DESC";  
 $result = $conn->query($sql1);
+
+
 
 echo '<h2 style="text-align: center; color: white;">Leaderboard</h1>';
 echo '<table style="margin-left: auto; margin-right: auto; margin-top: 50px; position: relative; width: calc(90vw - 2rem); max-width: 800px; border-spacing: 0 1rem; border: 1px solid rgba(255,255,255,0.3); color: white; border-collapse: collapse;">';
@@ -47,9 +62,11 @@ if ($result) {
     echo '<td style = "font-weight: 500; width: 10px; padding-left:2px;">';
     echo "$ctr</td>";
     echo '<td style = "padding-left: 12rem; font-size: 1.1rem; letter-spacing: 0.05rem; text-align:left">';
-    echo $row["first_name"]."</td>";
+    echo $row["username"]."</td>";
     echo '<td style = "font-size: 0.8rem;">';
-    echo $row["score"]."</td></tr>";
+    echo getRating('codechef', $row['ccname'])."</td></tr>";
+    echo '<td style = "font-size: 0.8rem;">';
+    echo getRating('codeforces', $row['cfname'])."</td></tr>";
     $ctr = $ctr + 1;
   }
 }
