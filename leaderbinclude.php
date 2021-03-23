6ecddb63a096242eaea38e2a66b70404aa261d4a
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-23 19:11:06 +0530

    Update leaderbinclude.php

--- leaderbinclude.php
+++ leaderbinclude.php
@@ -21,7 +21,7 @@ if($conn === false){
   echo "<br>";
 }
 
-$sql1 = "SELECT * FROM persons BY Score DESC";  
+$sql1 = "SELECT * FROM persons BY score DESC";  
 $result = $conn->query($sql1);
 
 echo '<h2 style="text-align: center; color: white;">Leaderboard</h1>';
