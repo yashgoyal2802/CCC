5bd69b91fdf6a8cdb14ada9c598573a2d63dfb5b
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-24 18:37:02 +0530

    Update leaderbinclude.php

--- leaderbinclude.php
+++ leaderbinclude.php
@@ -40,6 +40,12 @@ $result = $conn->query($sql1);
 
 
 echo '<h2 style="text-align: center; color: white;">Leaderboard</h1>';
+echo '<style>
+th {
+  cursor: pointer;
+}
+</style';
+
 echo '<table id="leader"; style="margin-left: auto; margin-right: auto; margin-top: 50px; position: relative; width: calc(90vw - 2rem); max-width: 800px; border-spacing: 0 1rem; border: 1px solid rgba(255,255,255,0.3); color: white; border-collapse: collapse;">';
 
 echo '<tr style="background-color: rgba(255, 255, 255, 0.3);">';
@@ -75,37 +81,57 @@ echo '</table>';
 echo "<br><br><br>";
 
 echo '<script>
-function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
   table = document.getElementById("leader");
   switching = true;
-  /* Make a loop that will continue until
-  no switching has been done: */
+  //Set the sorting direction to ascending:
+  dir = "desc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
   while (switching) {
-    // Start by saying: no switching is done:
+    //start by saying: no switching is done:
     switching = false;
-    rows = table.rows;
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
+      //start by saying there should be no switching:
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-      // Check if the two rows should switch place:
-      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
       }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
     }
   }
 }
