c072ffff85a91faaec3ca2c567d1ac022b1f9b8c
BlackHatGhost <yashdineshgoyal@gmail.com>
2021-03-24 15:22:20 +0530

    retrieving value
    username and password

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php 
+if(isset($_POST["login"]))
+{
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    // $ccname = $_POST["ccname"];
+    // $cfname = $_POST["cfname"];
+    // $email = $_POST["email"];
+}
+
+
+?>
