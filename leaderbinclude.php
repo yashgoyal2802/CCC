8e7c2f6677ff99dfdfbe9acbd240aaacb412b738
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-24 18:40:17 +0530

    style tag fix

--- leaderbinclude.php
+++ leaderbinclude.php
@@ -44,7 +44,7 @@ echo '<style>
 th {
   cursor: pointer;
 }
-</style';
+</style>';
 
 echo '<table id="leader"; style="margin-left: auto; margin-right: auto; margin-top: 50px; position: relative; width: calc(90vw - 2rem); max-width: 800px; border-spacing: 0 1rem; border: 1px solid rgba(255,255,255,0.3); color: white; border-collapse: collapse;">';
 
