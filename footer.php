6ff31c2a1a401f379b335789afb6740031fb0269
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-25 14:23:18 +0530

    footer

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+  echo'
+      <div class="footer-basic">
+      <footer>
+        <div class="social">
+          <a href="#"><i class="icon ion-social-instagram"></i></a
+          ><a href="#"><i class="icon ion-social-snapchat"></i></a
+          ><a href="#"><i class="icon ion-social-twitter"></i></a
+          ><a href="#"><i class="icon ion-social-facebook"></i></a>
+        </div>
+        <ul class="list-inline">
+          <li class="list-inline-item"><a href="home.html">Home</a></li>
+          <li class="list-inline-item"><a href="contests.html">Calendar</a></li>
+          <li class="list-inline-item">
+            <a href="leaderboard.php">Leader Board</a>
+          </li>
+          <li class="list-inline-item">
+            <a href="achievements.php">Achievements</a>
+          </li>
+          <li class="list-inline-item"><a href="#">Resources</a></li>
+        </ul>
+        <p class="copyright">Competitive Coding Club © 2021</p>
+      </footer>
+    </div>';
+?>
