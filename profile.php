cc213bf5ec5462fe307fa4b3f7352a813bc58cef
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-26 00:45:01 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,4 +1,38 @@
 <?php
+$user = $_COOKIE['username'];
+
+function OpenCon()
+{
+	$dbhost = "sql6.freesqldatabase.com";
+	$dbuser = "sql6400897";
+	$dbpass = "gcysFbCvd9";
+	$db = "sql6400897";
+	$conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
+ 	return $conn;
+}
+
+function CloseCon($conn)
+{
+    $conn -> close();
+} 
+
+$conn = OpenCon();
+if($conn === false){
+  die("ERROR: Could not connect." . $conn->connect_error);
+  echo "<br>";
+}
+
+$sql = "SELECT * FROM persons WHERE username='$user'";
+$result = $conn -> query($sql);
+
+if($result){
+  
+  $email = $result['email'];
+  $ccname = $result['ccname'];
+  $cfname = $result['cfname'];
+  $ach = $result['achievements'];
+  $pwd = $result['password'];
+}
 echo '<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,7 +68,7 @@ echo '<!DOCTYPE html>
                     <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
                       <div class="text-center text-sm-left mb-2 mb-sm-0">
 
-                      	<h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">$user</h4>;
+                      	<h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">$user</h4>
 
                       </div>
                     </div>
