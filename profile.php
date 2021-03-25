adabb0d94b3dbef3bbb7b7e9db0f5f72a32a60e7
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-26 00:58:34 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -38,6 +38,20 @@ echo "
 <head>
   <link rel='icon' href='images/ccc.png'>
   <meta charset='utf-8'>
+  <link
+    rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css'
+  />
+  <link
+    rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css'
+  />
+  <link rel='stylesheet' href='Navigation-Clean.css' />
+  <link rel='stylesheet' href='footer.css' />
+  <link
+    rel='stylesheet'
+    href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
+  />
   <title>Profile</title>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
   <link href='https://netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet'>
