4ebb2400a61d3ce75ecb0474639cb5bccff06286
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-25 14:16:00 +0530

    navbar

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<?php
+  echo'
+  <nav class="navbar navbar-light navbar-expand-md navigation-clean mynav">
+          <img src="images/ccc.png" alt="logo" width="5%" />
+          <div class="container">
+            <a class="navbar-brand" href="#">Competitive Coding Club</a>
+            <button
+              data-toggle="collapse"
+              class="navbar-toggler"
+              data-target="#navcol-1"
+            >
+              <span class="sr-only">Toggle navigation</span>
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+              <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="#">Resources</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="contests.html">Calendar</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="leaderboard.php"
+                    >Leader Board</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="achievements.php"
+                    >Achievements</a
+                  >
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="#">My profile</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link mylink" href="#">Logout</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>';
+?>
