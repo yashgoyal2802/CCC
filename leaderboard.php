a93a218fc0f6c38e69002e2e8304131aace15a5d
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-25 14:51:15 +0530

    meta tag, style

--- leaderboard.php
+++ leaderboard.php
@@ -3,9 +3,20 @@
 
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="Navigation-Clean.css">
-    <link rel="stylesheet" href="footer.css">    
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+    />
+    <link rel="stylesheet" href="Navigation-Clean.css" />
+    <link rel="stylesheet" href="footer.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+    />  
     <style>
 
         .content {
