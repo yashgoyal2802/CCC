f0f12349b3e8fcb9071fc21ab95202614241f766
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-25 23:48:19 +0530

    Update resources.php

--- resources.php
+++ resources.php
@@ -9,6 +9,10 @@
       href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css"
     />
     <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
     />
