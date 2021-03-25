871d20c46f57ebcac6cb12aa8c144ed9c69886ff
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-25 16:55:09 +0530

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if($result->num_rows>0){
 	$cookie_name = "username"; 
 	$cookie_value = $username;
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
-	header('Location: home.html');
+	header('Location: home.php');
 }else{
     $error = "Your Login Name or Password is invalid";
     echo "$error";
