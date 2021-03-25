dec5297321714100054babf9c064524a5ea646b6
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-26 00:52:30 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -33,143 +33,139 @@ if($result){
   $ach = $result['achievements'];
   $pwd = $result['password'];
 }
-echo '<!DOCTYPE html>
-<html lang="en">
+echo "
+<html lang='en'>
 <head>
-  <link rel="icon" href="images/ccc.png">
-  <meta charset="utf-8">
+  <link rel='icon' href='images/ccc.png'>
+  <meta charset='utf-8'>
   <title>Profile</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="http://netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet">
-  <style type="text/css">
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link href='http://netdna.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet'>
+  <style type='text/css'>
     body {
       margin-top: 20px;
       background: #f8f8f8
     }
   </style>
 </head>
-
 <body>
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-  <div class="container">
-    <div class="row flex-lg-nowrap">
-      <div class="col-12 col-lg-auto mb-3" style="width: 200px;">
+  <link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'>
+  <div class='container'>
+    <div class='row flex-lg-nowrap'>
+      <div class='col-12 col-lg-auto mb-3' style='width: 200px;'>
       </div>
-
-      <div class="col">
-        <div class="row">
-          <div class="col mb-3">
-            <div class="card">
-              <div class="card-body">
-                <div class="e-profile">
-                  <div class="row">
-                    <div class="col-12 col-sm-auto mb-3">
+      <div class='col'>
+        <div class='row'>
+          <div class='col mb-3'>
+            <div class='card'>
+              <div class='card-body'>
+                <div class='e-profile'>
+                  <div class='row'>
+                    <div class='col-12 col-sm-auto mb-3'>
                     </div>
-                    <div class="col d-flex flex-column flex-sm-row justify-content-between mb-3">
-                      <div class="text-center text-sm-left mb-2 mb-sm-0">
-
-                      	<h4 class="pt-sm-2 pb-1 mb-0 text-nowrap">$user</h4>
-
+                    <div class='col d-flex flex-column flex-sm-row justify-content-between mb-3'>
+                      <div class='text-center text-sm-left mb-2 mb-sm-0'>
+                      	<h4 class='pt-sm-2 pb-1 mb-0 text-nowrap'>$user</h4>
                       </div>
                     </div>
                   </div>
-                  <ul class="nav nav-tabs">
-                    <li class="nav-item"><a href="" class="active nav-link">Settings</a></li>
+                  <ul class='nav nav-tabs'>
+                    <li class='nav-item'><a href='' class='active nav-link'>Settings</a></li>
                   </ul>
-                  <div class="tab-content pt-3">
-                    <div class="tab-pane active">
-                      <form action="update.php" method="POST">
-                        <div class="row">
-                          <div class="col">
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                  <div class='tab-content pt-3'>
+                    <div class='tab-pane active'>
+                      <form action='update.php' method='POST'>
+                        <div class='row'>
+                          <div class='col'>
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>Username</label>
-                                  <input class="form-control" type="text" name="usernname" value=$user readonly>
+                                  <input class='form-control' type='text' name='usernname' value=$user readonly>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>Email</label>
-                                  <input class="form-control" type="email" name="email" value=$email>
+                                  <input class='form-control' type='email' name='email' value=$email>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>Codechef Username</label>
-                                  <input class="form-control" type="text" name="ccname" value=$ccname>
+                                  <input class='form-control' type='text' name='ccname' value=$ccname>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>Codeforces Username</label>
-                                  <input class="form-control" type="text" name="cfname" value=$cfname>
+                                  <input class='form-control' type='text' name='cfname' value=$cfname>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col mb-3">
-                                <div class="form-group">
+                            <div class='row'>
+                              <div class='col mb-3'>
+                                <div class='form-group'>
                                   <label>Achievements</label>
-                                  <textarea class="form-control" rows="5" name="ach">$ach</textarea>
+                                  <textarea class='form-control' rows='5' name='ach'>$ach</textarea>
                                 </div>
                               </div>
                             </div>
                           </div>
                         </div>
-                        <div class="row">
-                          <div class="col-12 col-sm-6 mb-3">
-                            <div class="mb-2"><b>Change Password</b></div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                        <div class='row'>
+                          <div class='col-12 col-sm-6 mb-3'>
+                            <div class='mb-2'><b>Change Password</b></div>
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>Current Password</label>
-                                  <input class="form-control" type="password" name="cpwd" value=$pwd>
+                                  <input class='form-control' type='password' name='cpwd' value=$pwd>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
                                   <label>New Password</label>
-                                  <input class="form-control" type="password" name="npwd" value=$pwd>
+                                  <input class='form-control' type='password' name='npwd' value=$pwd>
                                 </div>
                               </div>
                             </div>
-                            <div class="row">
-                              <div class="col">
-                                <div class="form-group">
-                                  <label>Confirm <span class="d-none d-xl-inline">Password</span></label>
-                                  <input class="form-control" type="password" name="ccpwd" value=$pwd></div>
+                            <div class='row'>
+                              <div class='col'>
+                                <div class='form-group'>
+                                  <label>Confirm <span class='d-none d-xl-inline'>Password</span></label>
+                                  <input class='form-control' type='password' name='ccpwd' value=$pwd></div>
                               </div>
                             </div>
                           </div>
-                          <!-- <div class="col-12 col-sm-5 offset-sm-1 mb-3">
-                            <div class="mb-2"><b>Keeping in Touch</b></div>
-                            <div class="row">
-                              <div class="col">
+                          <!-- <div class='col-12 col-sm-5 offset-sm-1 mb-3'>
+                            <div class='mb-2'><b>Keeping in Touch</b></div>
+                            <div class='row'>
+                              <div class='col'>
                                 <label>Email Notifications</label>
-                                <div class="custom-controls-stacked px-2">
-                                  <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="notifications-blog"
-                                      checked="">
-                                    <label class="custom-control-label" for="notifications-blog">Blog posts</label>
+                                <div class='custom-controls-stacked px-2'>
+                                  <div class='custom-control custom-checkbox'>
+                                    <input type='checkbox' class='custom-control-input' id='notifications-blog'
+                                      checked=''>
+                                    <label class='custom-control-label' for='notifications-blog'>Blog posts</label>
                                   </div>
-                                  <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="notifications-news"
-                                      checked="">
-                                    <label class="custom-control-label" for="notifications-news">Newsletter</label>
+                                  <div class='custom-control custom-checkbox'>
+                                    <input type='checkbox' class='custom-control-input' id='notifications-news'
+                                      checked=''>
+                                    <label class='custom-control-label' for='notifications-news'>Newsletter</label>
                                   </div>
-                                  <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" id="notifications-offers"
-                                      checked="">
-                                    <label class="custom-control-label" for="notifications-offers">Personal
+                                  <div class='custom-control custom-checkbox'>
+                                    <input type='checkbox' class='custom-control-input' id='notifications-offers'
+                                      checked=''>
+                                    <label class='custom-control-label' for='notifications-offers'>Personal
                                       Offers</label>
                                   </div>
                                 </div>
@@ -177,9 +173,9 @@ echo '<!DOCTYPE html>
                             </div>
                           </div> -->
                         </div>
-                        <div class="row">
-                          <div class="col d-flex justify-content-end">
-                            <button class="btn btn-primary" type="submit" onclick="return validateForm()">Save Changes</button>
+                        <div class='row'>
+                          <div class='col d-flex justify-content-end'>
+                            <button class='btn btn-primary' type='submit' onclick='return validateForm()'>Save Changes</button>
                           </div>
                         </div>
                       </form>
@@ -189,39 +185,34 @@ echo '<!DOCTYPE html>
               </div>
             </div>
           </div>
-
-          <div class="col-12 col-md-3 mb-3">
+          <div class='col-12 col-md-3 mb-3'>
           </div>
         </div>
-
       </div>
     </div>
   </div>
-  <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-  <script type="text/javascript">
+  <script src='http://code.jquery.com/jquery-1.10.2.min.js'></script>
+  <script src='http://netdna.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js'></script>
+  <script type='text/javascript'>
   </script>
 <script>
   function validateForm(){
-
-    var cpwd = document.getElementById("cpwd").value; 
-    var npwd = document.getElementById("npwd").value;
-    var email = document.getElementById("email").value;
-    var ccpwd = document.getElementById("ccpwd").value;
+    var cpwd = document.getElementById('cpwd').value; 
+    var npwd = document.getElementById('npwd').value;
+    var email = document.getElementById('email').value;
+    var ccpwd = document.getElementById('ccpwd').value;
     var opwd = $pwd;
-
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+    const re = /^(([^<>()\[\]\\.,;:\s@']+(\.[^<>()\[\]\\.,;:\s@']+)*)|('.+'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(cpwd != opwd){
-      alert("Incorrect Current Password");
+      alert('Incorrect Current Password');
       return false;
     }
     else if(npwd != ccpwd){
-      alert("Passwords don\'t match");
+      alert('Passwords don\'t match');
       return false;
     }
     else if(!re.test(String(email).toLowerCase())){
-    	alert("Error: Enter a valid e-mail id");
+    	alert('Error: Enter a valid e-mail id');
     	return false;
     }
     
@@ -230,5 +221,5 @@ echo '<!DOCTYPE html>
 </script>
 </body>
 </html>
-';
+";
 ?>
