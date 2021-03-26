5ff9aaa254d5dd05ae4ec1a4a053c555463f68c4
Aryann Dhir <59336767+aryanndhir@users.noreply.github.com>
2021-03-26 11:10:54 +0530

    test

--- update.php
+++ update.php
@@ -7,34 +7,38 @@ if(isset($_POST["update"]))
     $cfname = $_POST["cfname"];
     $ach = $_POST["ach"];
     $pwd = $_POST["ccpwd"];
-
-    function OpenCon()
-    {
-        $dbhost = "sql6.freesqldatabase.com";
-        $dbuser = "sql6400897";
-        $dbpass = "gcysFbCvd9";
-        $db = "sql6400897";
-        $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
-        return $conn;
-    }
-
-    function CloseCon($conn)
-    {
-        $conn -> close();
-    } 
-
-    $conn = OpenCon();
-    if($conn === false){
-        die("ERROR: Could not connect." . $conn->connect_error);
-        echo "<br>";
-    }
-
-    $sql = "UPDATE persons SET username='$name', email='$email', ccname='$ccname', cfname='$cfname', achievements='$ach', password='$pwd' WHERE username='$name'";
     
-    if($conn->query($sql) === true){
-        header('Location: home.php');
-        exit;
-    }
-        CloseCon($conn);
+    echo $ccname;
+    header('Location: home.php');
+
+    
+//     function OpenCon()
+//     {
+//         $dbhost = "sql6.freesqldatabase.com";
+//         $dbuser = "sql6400897";
+//         $dbpass = "gcysFbCvd9";
+//         $db = "sql6400897";
+//         $conn = new mysqli($dbhost, $dbuser, $dbpass,$db);
+//         return $conn;
+//     }
+
+//     function CloseCon($conn)
+//     {
+//         $conn -> close();
+//     } 
+
+//     $conn = OpenCon();
+//     if($conn === false){
+//         die("ERROR: Could not connect." . $conn->connect_error);
+//         echo "<br>";
+//     }
+
+//     $sql = "UPDATE persons SET username='$name', email='$email', ccname='$ccname', cfname='$cfname', achievements='$ach', password='$pwd' WHERE username='$name'";
+    
+//     if($conn->query($sql) === true){
+//         header('Location: home.php');
+//         exit;
+//     }
+//         CloseCon($conn);
 }
 ?>
