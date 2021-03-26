55cfb2f22c8e09b209fb663a6b8375c80a40d685
disc0nnctd <Divyanshchhablani00@gmail.com>
2021-03-26 15:57:42 +0530

    Update and rename contests.html to contests.php

--- contests.php
+++ contests.php
@@ -89,25 +89,7 @@
 </head>
 <body>
     <header>
-        <nav class="navbar navbar-light navbar-expand-md navigation-clean mynav">
-            <img src="images/ccc.png" alt="logo" width="5%">
-            <div class="container"><a class="navbar-brand" href="#">Competitive Coding Club</a>
-                <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navcol-1">
-                    <ul class="nav navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link mylink" href="#">Resources</a></li>
-                        <li class="nav-item"><a class="nav-link mylink" href="contests.html">Calendar</a></li>
-                        <li class="nav-item"><a class="nav-link mylink" href="leaderboard.php">Leader Board</a></li>
-                        <li class="nav-item"><a class="nav-link mylink" href="achievements.php">Achievements</a></li>
-                        <li class="nav-item"><a class="nav-link mylink" href="#">My profile</a></li>
-                        <li class="nav-item"><a class="nav-link mylink" href="#">Logout</a></li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <?php include('navbar.php')?>
     </header>
     <iframe src="https://calendar.google.com/calendar/embed?src=8a4ko50nq55ma5smhuhdp5rpmaek45lg%40import.calendar.google.com&ctz=Asia%2FKolkata" style="border: 0; margin-top: 6px" width="98%" height="600" frameborder="0" scrolling="no"></iframe>
     <br><br>
