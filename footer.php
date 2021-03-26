cfed8257adf3d2d0c19418bc556b856dd42bda5c
Yash Goyal <yashdineshgoyal@gmail.com>
2021-03-26 23:24:15 +0530

    dicord link update

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
       <div class="footer-basic">
       <footer>
         <div class="social">
-          <a href="https://discord.gg/qNccSFp" target="_blank" style="line-height: 41px;font-size: 22px;"><ion-icon name="logo-discord"></ion-icon></a
+          <a href="https://discord.com/invite/MCNujFq" target="_blank" style="line-height: 41px;font-size: 22px;"><ion-icon name="logo-discord"></ion-icon></a
           ><a href="https://chat.whatsapp.com/JTYNYuHb87MG3fTgy07AlL" target="_blank" ><i class="icon ion-social-whatsapp"></i></a
           ><a href="https://github.com/Competitive-Coding-Club" target="_blank" ><i class="icon ion-social-github"></i></a
           ><a href="https://www.linkedin.com/company/competitivecodingclub/" target="_blank" ><i class="icon ion-social-linkedin"></i></a>
