a4abb9a2d1044f5964a591866f75b13ef95f153a
Yash Goyal <yashdineshgoyal@gmail.com>
2021-03-29 15:38:19 +0530

    Update profile.php

--- profile.php
+++ profile.php
@@ -159,7 +159,7 @@ echo"</header>
                                 <div class='form-group'>
                                   <label>Current Password</label>
                                   <input class='form-control' type='password' name='cpwd' value=$pwd>
-				  <i class='icon ion-eye' id='togglePassword' style='margin-left: 255px; margin-top: 15px; color: 'gray'; cursor: pointer'></i>
+				  <i class='icon ion-eye' id='togglePassword' style='margin-left: 255px; margin-top: 15px; color: gray; cursor: pointer'></i>
                                 </div>
                               </div>
                             </div>
@@ -168,7 +168,7 @@ echo"</header>
                                 <div class='form-group'>
                                   <label>New Password</label>
                                   <input class='form-control' type='password' name='npwd' value=$pwd>
-				  <i class='icon ion-eye' id='togglePassword1' style='margin-left: 255px; margin-top: 15px; color: 'gray'; cursor: pointer'></i>
+				  <i class='icon ion-eye' id='togglePassword1' style='margin-left: 255px; margin-top: 15px; color: gray; cursor: pointer'></i>
                                 </div>
                               </div>
                             </div>
@@ -177,7 +177,7 @@ echo"</header>
                                 <div class='form-group'>
                                   <label>Confirm <span class='d-none d-xl-inline'>Password</span></label>
                                   <input class='form-control' type='password' name='ccpwd' value=$pwd>
-				  <i class='icon ion-eye' id='togglePassword2' style='margin-left: 255px; margin-top: 15px; color: 'gray'; cursor: pointer'></i>
+				  <i class='icon ion-eye' id='togglePassword2' style='margin-left: 255px; margin-top: 15px; color: gray; cursor: pointer'></i>
 				 </div>
                               </div>
                             </div>
